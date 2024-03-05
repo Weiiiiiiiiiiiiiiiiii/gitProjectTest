b6f4da84bb39bd5d6789ad8012731a2b555ce96a
Ryan <wei900709@gmail.com>
2024-03-05 09:58:38 +0800

    測試資料_v_3.0

--- projecrt_shop/back/conn.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $server = "localhost";
-    $user = "root";
-    $pwd = "";
-    $db = "project";
-
-    $conn = new mysqli($server, $user, $pwd, $db);
-    mysqli_query($conn, "SET NAMES utf8");
-?>
